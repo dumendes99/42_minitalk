fd297de807daa6009acbbb15269567b15984e376
Eduardo Mendes <dum3ndes@gmail.com>
2021-09-20 21:23:27 -0300

    to test on guaca

--- sources_client/client.c
+++ sources_client/client.c
@@ -6,19 +6,77 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 12:50:02 by elima-me          #+#    #+#             */
-/*   Updated: 2021/09/17 13:35:15 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/09/20 21:17:20 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
-int main(int argc, char *argv[])
+void	get_char(int pid, unsigned int c)
 {
+	int count;
+	char bin[9];
+	int i = 7;
+
+	count = 0;
+	bin[8] = '\0';
+	while (count < 8)
+	{
+		if (c % 2 == 0 || c == 0)
+		{
+			kill(pid, SIGUSR1);
+			sleep(1);
+			bin[i] = '0';
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+			sleep(1);
+			bin[i] = '1';
+		}
+		c /= 2;
+		printf("count client == %d\n", count);
+		count++;
+		i--;
+	}
+	// printf("%s\n", bin);
+}
+
+int	get_id(char *arg)
+{
+	int id;
+
+	id = 0;
+	while (*arg)
+	{
+		if (*arg >= '0' && *arg <= '9')
+			id = (id * 10) + (*arg - '0');
+		else 
+			return (0);
+		arg++;
+	}
+	return (id);
+}
+
+int main(int argc, char *argv[])
+{	
+	pid_t pid;
+	
 	if (argc != 3)
 	{
-		ft_printf("Please inform PID, and the string witch you want send!\n");
+		ft_printf("Please inform PID, and the string wich you want send!\n");
 		return (0);
 	}
-	printf("%s\n", argv[1]);
+	pid = get_id(argv[1]);
+	ft_printf("%d\n", pid);
+	if (!pid)
+		ft_printf("Please inform a correct PID!!\n");
+	while(*argv[2])
+	{
+		get_char(pid, *argv[2]);
+		ft_printf("%c\n", *argv[2]);
+		argv[2]++;
+	}
+	ft_printf("\n");
 	return (1);
 }
--- sources_server/server.c
+++ sources_server/server.c
@@ -6,17 +6,52 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 12:05:55 by elima-me          #+#    #+#             */
-/*   Updated: 2021/09/17 12:44:11 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/09/20 21:22:29 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
+void handler(int sig)
+{
+	static unsigned int	count;
+	static unsigned int letter;
+
+	ft_printf("count server == %d\n", count);
+	// ft_printf("letter == %d\n", letter);
+	if (count > 7)
+	{
+		count = 0;
+		letter = 0;
+	}
+	if (sig == SIGUSR2)
+	{
+		letter += 1 << count;
+		// ft_printf("1\n");
+	}
+	if (sig == SIGUSR1)
+		// ft_printf("0\n");
+	count++;
+	// if (count == 8)
+		// ft_printf("%d", letter);
+}
+
 int main(void)
 {
 	int pid;
+	struct sigaction sa;
 
+	sa.sa_handler = &handler;
+	sa.sa_flags = 0;
 	pid = getpid();
 	ft_printf("process id: %d\n", pid);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+	{
+	}
 	return (0);
 }
+
+// o binario vai chegar aqui, tenho que colocar ele dentro de uma variavel static para poder mecher na estrutura binaria dela sempre que entrar nessa função. 
+// tenho que ter outra variavel static para saber que fui até 8;
